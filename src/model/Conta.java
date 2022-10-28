a69593b8db9852eff6f19fb24880915416fc7064
Gleison Mozart <mozart.gleison@gmail.com>
2022-10-27 21:22:15 -0300

    Implementando funcionalidade de crédito

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -1,32 +1,36 @@
 package model;
 
-public class Conta implements ContaInterface {
+public abstract class Conta implements ContaInterface {
     private int agencia;
     private int numero;
     private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-    public int getNumero() {
-        return numero;
-    }
-    public double getSaldo() {
-        return saldo;
-    }
-
     @Override
     public void sacar(Double valor) {
 
     }
-
     @Override
     public void depositar(Double valor) {
 
     }
-
     @Override
     public void transferir(Double valor, Conta contaDestino) {
 
+    }
+
+    @Override
+    public void linhaCredito(Double valor, String tipoConta) {
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
