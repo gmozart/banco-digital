c76cc95ecea00aa8784f7a69fbb3494cf0ebe551
Gleison Mozart <mozart.gleison@gmail.com>
2022-10-27 21:30:53 -0300

    Implementando funcionalidades da linha de crédito

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -4,13 +4,17 @@ public abstract class Conta implements ContaInterface {
     private int agencia;
     private int numero;
     private double saldo;
+    private double credito;
     @Override
     public void sacar(Double valor) {
-
+        saldo -= valor;
+    }
+    public void sacarCredito(Double valor) {
+        credito -= valor;
     }
     @Override
     public void depositar(Double valor) {
-
+        saldo += valor;
     }
     @Override
     public void transferir(Double valor, Conta contaDestino) {
@@ -20,6 +24,11 @@ public abstract class Conta implements ContaInterface {
     @Override
     public void linhaCredito(Double valor, String tipoConta) {
 
+        if(tipoConta == "CC"){
+            credito += valor;
+        }else {
+            credito = 0;
+        }
     }
 
     public int getAgencia() {
